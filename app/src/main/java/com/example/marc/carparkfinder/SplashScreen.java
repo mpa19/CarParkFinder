5fc501316089eaa1ac9bbb21c0f7059a5cba1ebe
mpa19 <marcperezarnaiz@gmail.com>
2020-03-21 02:26:09 +0100

    first steps HomePage

--- app/src/main/java/com/example/marc/carparkfinder/SplashScreen.java
+++ app/src/main/java/com/example/marc/carparkfinder/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.marc.carparkfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+    }
+
+    public void onlick(View v){
+        Intent i = new Intent(this, HomePage.class);
+        startActivity(i);
     }
 }
