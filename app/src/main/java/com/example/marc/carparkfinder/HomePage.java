cb6646fc6055dcd53f3434873b519f6f642b32ca
mpa19 <marcperezarnaiz@gmail.com>
2020-03-21 15:51:54 +0100

    first steps Details page

--- app/src/main/java/com/example/marc/carparkfinder/HomePage.java
+++ app/src/main/java/com/example/marc/carparkfinder/HomePage.java
@@ -2,7 +2,9 @@ package com.example.marc.carparkfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void btnR(View v){
+        Intent i = new Intent(this, DetailsParking.class);
+        startActivity(i);
     }
 }
