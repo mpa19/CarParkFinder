f22dd37c450b057c444f7e383e8793a327b1574f
mpa19 <marcperezarnaiz@gmail.com>
2020-04-12 16:04:58 +0200

    fixed message GPS

--- app/src/main/java/com/example/marc/carparkfinder/MapsActivity.java
+++ app/src/main/java/com/example/marc/carparkfinder/MapsActivity.java
@@ -110,16 +110,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void buildAlertMessageNoGps() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+        builder.setMessage(R.string.GPS)
                 .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Activar-lo", new DialogInterface.OnClickListener() {
                     public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
                         startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        dialog.cancel();
                     }
                 });
         final AlertDialog alert = builder.create();
