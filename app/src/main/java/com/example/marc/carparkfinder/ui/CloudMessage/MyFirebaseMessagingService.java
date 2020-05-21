2fe11c5e7390d0c6252412f64f2373aadf1eca2f
mpa19 <marcperezarnaiz@gmail.com>
2020-05-21 21:15:46 +0200

    Check if have reservation

--- app/src/main/java/com/example/marc/carparkfinder/ui/CloudMessage/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/marc/carparkfinder/ui/CloudMessage/MyFirebaseMessagingService.java
@@ -4,17 +4,25 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.view.View;
 
 import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.DialogFragment;
 
 import com.example.marc.carparkfinder.R;
 import com.example.marc.carparkfinder.ui.Reserva.ReservarActivity;
+import com.example.marc.carparkfinder.ui.Reserva.TimerHelper.TimePickerFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public static final String NOTIFICATION_CHANNEL_ID = "10001";
-
+    private Intent resultIntent;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
@@ -29,38 +37,53 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
     }
 
-    private void showNotification(String title, String campus) {
-        NotificationCompat.Builder mBuilder =
+    private void showNotification(String title, final String campus) {
+        final NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle(title+campus);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-        Intent resultIntent = new Intent(this, ReservarActivity.class);
-        if(campus.equals(getResources().getString(R.string.cap))){
-            resultIntent.putExtra("Campus", 1);
-        } else resultIntent.putExtra("Campus", 2);
+        FirebaseUser user = mAuth.getCurrentUser();
 
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        this,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+        db.collection("Reserva")
+                .document(user.getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (!documentSnapshot.exists()) {
 
-        mBuilder.setContentIntent(resultPendingIntent);
-        int mNotificationId = (int) System.currentTimeMillis();
-        NotificationManager mNotifyMgr =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
-        {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+                            resultIntent = new Intent(getApplication(), ReservarActivity.class);
+                            if (campus.equals(getResources().getString(R.string.cap))) {
+                                resultIntent.putExtra("Campus", 1);
+                            } else resultIntent.putExtra("Campus", 2);
 
-            mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
-            mNotifyMgr.createNotificationChannel(notificationChannel);
-        }
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
+                            PendingIntent resultPendingIntent =
+                                    PendingIntent.getActivity(
+                                            getApplicationContext(),
+                                            0,
+                                            resultIntent,
+                                            PendingIntent.FLAG_UPDATE_CURRENT
+                                    );
+
+                            mBuilder.setContentIntent(resultPendingIntent);
+                            int mNotificationId = (int) System.currentTimeMillis();
+                            NotificationManager mNotifyMgr =
+                                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                                int importance = NotificationManager.IMPORTANCE_HIGH;
+                                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+
+                                mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
+                                mNotifyMgr.createNotificationChannel(notificationChannel);
+                            }
+                            mNotifyMgr.notify(mNotificationId, mBuilder.build());
+                        }
+                    }
+                });
     }
 }
 
